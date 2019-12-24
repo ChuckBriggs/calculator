50ddce09df4650cc386c780d5d15644f27de3764
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2019-12-24 16:06:32 -0600

    js for operate and operator functions working

---
+++ script.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	if (b) {
+		return a / b;
+	} else {
+		console.log('The universe implodes.');
+	}
+}
+
+function operate(operator, a, b) {
+	switch(operator) {
+		case '+':
+			return add(a,b);
+			break;
+		case '-':
+			return subtract(a,b);
+			break;
+		case '*':
+			return multiply(a,b);
+			break;
+		case '/':
+			return divide(a,b);
+			break;
+		default:
+			console.log('Sorry, we are out of ' + operator + '.');
+	}
+}
