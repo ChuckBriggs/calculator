a79f848ab9a6f832e4b2303e1d2cd35a2bdce7bc
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2020-01-04 17:18:51 -0600

    only allows valid strings, parses correctly, parses string correctly. hangs on something - i think minus, just add keyboard support and see if can figure out where it hangs.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let displayValue = '1234567890';
+let displayValue = '0';
+
+const display = document.querySelector('#calcDisplay');
 
 function add(a, b) {
 	return a + b;
@@ -38,3 +40,150 @@ function operate(operator, a, b) {
 			console.log('Sorry, we are out of ' + operator + '.');
 	}
 }
+
+function updateDisplay() {
+	display.textContent = displayValue;
+}
+
+function addToDisplay(e) {
+	if (displayValue == '0') {
+		displayValue = '';
+	}
+	displayValue += this.value;
+	updateDisplay();
+}
+
+function checkValidity(e) {
+	let a = displayValue.slice(-1);
+	if (a >= '0' && a <='9') {
+		if (this.value === '.') {
+			for (i = -1; a >= '0' && a <= '9'; i--) {
+				a = displayValue.slice(i-1, i);
+			}
+			if (a != '.') {
+				displayValue += this.value;
+			}
+		} else {
+			displayValue += this.value;
+		}
+	}
+	updateDisplay();
+}
+
+function clearAll() {
+	displayValue = '0';
+	updateDisplay();
+}
+
+function deleteLast() {
+	if (displayValue.length > 1) {
+		displayValue = displayValue.slice(0, -1);
+	} else {
+		displayValue = 0;
+	}
+	updateDisplay();
+}
+
+function convertToNum(string) {
+	for (j = 0; j < string.length; j++) {
+		if (string[j] === '.') {
+			return parseFloat(string);
+		}
+	}
+	return parseInt(string);
+}
+
+function evaluateEquation() {
+	let a = displayValue.slice(-1);
+
+	if (a >= '0' && a <= '9') {
+		let operator = [];
+		let operand = [];
+		let index = 0;
+		for (i = 0; i < displayValue.length; i++) {
+			console.log(displayValue[i]);
+			if (displayValue[i] >= '0' && displayValue[i] <= '9' || displayValue[i] == '.') {
+				if (operand[index]) {
+					operand[index] += displayValue[i];
+				} else {
+					operand[index] = displayValue[i];
+				}
+			} else {
+				operator[index] = displayValue[i];
+				index++;
+			}
+		}
+		while (operator[0]) {
+			for (i = 0; i < operator.length; i++) {
+				if (operator[i] == '\xd7') {
+					let tempNum = operate('*', convertToNum(operand[i]), convertToNum(operand[i+1]));
+					operand[i+1] = tempNum.toString();
+					operator.splice(i, 1);
+					operand.splice(i, 1);
+					i = -1;
+				}
+				if (operator[i] == '/') {
+					let tempNum = operate('/', convertToNum(operand[i]), convertToNum(operand[i+1]));
+					operand[i+1] = tempNum.toString();
+					operator.splice(i, 1);
+					operand.splice(i, 1);
+					i = -1;
+				}
+			}
+			for (i = 0; i < operator.length; i++) {
+				if (operator[i] == '+') {
+					let tempNum = operate('+', convertToNum(operand[i]), convertToNum(operand[i+1]));
+					operand[i+1] = tempNum.toString();
+					operator.splice(i, 1);
+					operand.splice(i, 1);
+					i = -1;
+				}
+				if (operator[i] == '\u2212') {
+					let tempNum = operate('-', convertToNum(operand[i]), convertToNum(operand[i+1]));
+					operand[i+1] = tempNum.toString();
+					operator.splice(i, 1);
+					operand.splice(i, 1);
+					i = -1;
+				}
+			}
+		}
+		displayValue = operand[0];
+		updateDisplay();
+		console.log(operand);
+		console.log(operator);
+	} else {
+		console.log('no eval');
+	}
+console.log('testing123');
+//					console.log('operand[i]:');
+//					console.log(operand[i]);
+//					console.log('toNum operand[i]:');
+//					console.log(convertToNum(operand[i]));
+//					console.log('operand[i+1]:');
+//					console.log(operand[i+1]);
+//					console.log('toNum operand[i+1]:');
+//					console.log(convertToNum(operand[i+1]));
+//					console.log('tempNum:');
+//					console.log(tempNum);
+
+	//c check if last character is number
+	// check if there are operators
+	// if there are mult or div operators, do them
+	// if there are add or sub operators, do them 
+
+	updateDisplay();
+}
+
+const numBtns = document.querySelectorAll('.numBtn');
+numBtns.forEach(button =>{
+	button.addEventListener('click', addToDisplay);
+})
+const funcBtns = document.querySelectorAll('.funcBtn');
+funcBtns.forEach(button =>{
+	button.addEventListener('click', checkValidity);
+})
+document.querySelector('#clearBtn').addEventListener('click', clearAll);
+document.querySelector('#deleteBtn').addEventListener('click', deleteLast);
+document.querySelector('#eqBtn').addEventListener('click', evaluateEquation);
+
+updateDisplay();
